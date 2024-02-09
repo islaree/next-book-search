1dc39816fadf628ea0d7f7a8ae42b6405d255600
daxchx <daxchx.v1@gmail.com>
2024-02-10 00:24:46 +0900

    Update new design

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 
-export default function Home() {
+export default function RootPage() {
   return (
     <>
-      <div>welcome come to Books App</div>
-      <div>
-        this app is easy to searching books
+      <div className="flex justify-center items-center mt-8">
+        <img className="border border-gray-300 rounded-lg shadow-sm" src="demo.gif" />
+      </div>
+      <div className="mt-4 px-6 text-3xl text-gray-600">本を検索</div>
+      <div className="mt-4 px-6 text-lg text-gray-600">
+        Google Books APIを使用した本の検索機能の実装。
         <br />
       </div>
-      <div className="flex flex-col gap-y-4">
-        <Link href="/search">本を検索してみる</Link>
-        <Link href="/library">マイライブラリを確認する</Link>
+      <div className="mt-6 px-6">
+        <Link href="/search" className="inline-flex px-5 py-2.5 text-md text-white font-semibold bg-emerald-500 rounded-full">
+          本の検索はこちら
+        </Link>
       </div>
     </>
   )
