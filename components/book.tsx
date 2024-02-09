a5c21cff64522ec05a0132568f8a95fa600dd1eb
daxchx <daxchx.v1@gmail.com>
2024-02-09 23:07:24 +0900

    Add books component

--- components/book.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export default function Book({ smallThumbnail }: { smallThumbnail: string }) {
-  return <div>{smallThumbnail !== '404' ? <img src={smallThumbnail} /> : <div>not found 404</div>}</div>
-}
