77ca957000a2ef54abda5d7bbfaf77bc06b985c5
daxchx <daxchx.v1@gmail.com>
2024-02-09 23:05:12 +0900

    Add search/layout file

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,17 @@
-'use client'
-
-import Books from '@/components/books'
-import { Suspense, useState } from 'react'
-import Loading from '@/dashboard/loading'
+import Link from 'next/link'
 
 export default function Home() {
-  const [value, setValue] = useState('')
-  const [keyword, setKeyword] = useState('')
-
-  function handleClick() {
-    setKeyword(value)
-    setValue('')
-  }
-
   return (
     <>
-      <main>
-        <div className="pt-10 px-4 border-b border-gray-200">
-          <div className="mb-2 text-xs text-gray-400">キーワードから本を検索する</div>
-          <input
-            className="w-full px-4 py-2 bg-gray-50 border shadow-sm border-gray-300 rounded-lg focus:outline-emerald-400 active:outline-emerald-400 focus-visible:outline-emerald-400"
-            placeholder="検索"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <div className="flex justify-center pb-4">
-            <button className="mt-4 px-4 py-2 bg-gray-900 rounded-lg shadow-md text-sm text-white" onClick={handleClick}>
-              検索する
-            </button>
-          </div>
-        </div>
-        {keyword.length > 0 && (
-          <Suspense fallback={<Loading />}>
-            <Books value={keyword} />
-          </Suspense>
-        )}
-      </main>
+      <div>welcome come to Books App</div>
+      <div>
+        this app is easy to searching books
+        <br />
+      </div>
+      <div className="flex flex-col gap-y-4">
+        <Link href="/search">本を検索してみる</Link>
+        <Link href="/library">マイライブラリを確認する</Link>
+      </div>
     </>
   )
 }
