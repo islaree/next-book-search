60c093a94a09d7a444b4c38f2a84c198b18b349e
daxchx <daxchx.v1@gmail.com>
2024-02-10 13:39:06 +0900

    Add drawer menu

--- components/client/header.tsx
+++ components/client/header.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import { RiMenuFill } from 'react-icons/ri'
+import { MouseEventHandler } from 'react'
 
-export default function Header() {
+export default function Header({ isActive, onClick }: { isActive: boolean; onClick: MouseEventHandler<HTMLDivElement> }) {
   return (
     <header className="">
       <div className="flex justify-between items-center px-6 h-16 border-b border-gray-200">
@@ -11,9 +14,17 @@ export default function Header() {
           <div className="text-lg font-medium text-[#1c2b33] leading-none">Books</div>
         </Link>
         <div className="flex flex-col">
-          <div>
+          <div onClick={onClick}>
             <RiMenuFill className="w-6 h-6" />
           </div>
+        </div>
+      </div>
+      <div className={`fixed flex flex-col z-20 w-full h-[100vh] bg-white transition-all delay-200 ${isActive ? 'visible opacity-100' : 'invisible opacity-0'}`}>
+        <div onClick={onClick}>
+          <Link href="/">ホーム</Link>
+        </div>
+        <div onClick={onClick}>
+          <Link href="/search">検索</Link>
         </div>
       </div>
     </header>
