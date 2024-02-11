e7dbbb88b609b6393d9706db5904b959c9b32028
daxchx <daxchx.v1@gmail.com>
2024-02-12 02:28:26 +0900

    Update button text

--- components/client/like-button.tsx
+++ components/client/like-button.tsx
@@ -19,18 +19,18 @@ export default function LikeButton({ id }: { id: string }) {
   return (
     <>
       {data.indexOf(id) == -1 ? (
-        <button onClick={handleAddClick} className="flex gap-x-1 items-center">
+        <button onClick={handleAddClick} className="flex items-center gap-x-1">
           <IconContext.Provider value={{ color: '#83888c', size: '20px' }}>
             <PiHeartBold />
           </IconContext.Provider>
-          <div className="text-gray-500 font-medium text-md">ブックマークに追加</div>
+          <div className="text-md font-medium text-gray-500">マイライブラリに追加</div>
         </button>
       ) : (
-        <button onClick={handleRemoveClick} className="flex gap-x-1 items-center">
+        <button onClick={handleRemoveClick} className="flex items-center gap-x-1">
           <IconContext.Provider value={{ color: '#eb4667', size: '20px' }}>
             <AiFillHeart />
           </IconContext.Provider>
-          <div className="text-[#eb4667] text-md font-medium">ブックマークから削除</div>
+          <div className="text-md font-medium text-[#eb4667]">マイライブラリから削除</div>
         </button>
       )}
     </>
