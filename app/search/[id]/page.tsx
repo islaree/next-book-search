08702bef5f6e4afcc35e5bff4f5637dd6b2b66df
daxchx <daxchx.v1@gmail.com>
2024-02-12 00:17:58 +0900

    Add use context

--- app/search/[id]/page.tsx
+++ app/search/[id]/page.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link'
-import Book from '@/components/server/book'
 import { IoArrowBack } from 'react-icons/io5'
+import Book from '@/components/server/book'
 
-export default function SearchItemPage({ params }: { params: { id: string } }) {
+export default function Library({ params }: { params: { id: string } }) {
   return (
-    <div className="p-6">
+    <div className="relative">
+      <Book id={params.id} />
       <Link href="/search" className="inline-flex items-center gap-x-1">
         <IoArrowBack />
         <span className="leading-none text-sm">検索に戻る</span>
       </Link>
-      <Book id={params.id} />
     </div>
   )
 }
