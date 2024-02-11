dfe8b33218a2c1d7c54de66112a187979c9eea9f
daxchx <daxchx.v1@gmail.com>
2024-02-12 02:29:35 +0900

    Update new design

--- components/my-library/book.tsx
+++ components/my-library/book.tsx
@@ -1,4 +1,6 @@
 import { cache } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const getData = cache(async (bookId: string) => {
   const res = await fetch(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
@@ -10,8 +12,27 @@ export default async function Book({ id }: { id: string }) {
   const data = await getData(id)
 
   return (
-    <>
-      <div>{data.volumeInfo.title}</div>
-    </>
+    <Link href={`/search/${id}`}>
+      <div className="flex gap-x-2">
+        <div className="shrink-0">
+          {data.volumeInfo.hasOwnProperty('imageLinks') ? (
+            <Image
+              className="h-auto w-full rounded border border-slate-200"
+              src={data.volumeInfo.imageLinks.thumbnail}
+              width={80}
+              height={112}
+              alt={data.volumeInfo.title}
+            />
+          ) : (
+            <div>not image</div>
+          )}
+        </div>
+        <div>
+          <div className="text-sm font-bold text-slate-900">{data.volumeInfo.title}</div>
+          <div className="mt-2 text-xs text-slate-500">寄稿者 : {data.volumeInfo.authors}</div>
+          <div className="mt-1 text-xs text-slate-500">出版社 : {data.volumeInfo.publisher}</div>
+        </div>
+      </div>
+    </Link>
   )
 }
--- components/server/book.tsx
+++ components/server/book.tsx
@@ -1,6 +1,7 @@
 import { cache } from 'react'
 import parse from 'html-react-parser'
 import LikeButton from '@/components/client/like-button'
+import Image from 'next/image'
 
 const getData = cache(async (bookId: string) => {
   const res = await fetch(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
@@ -14,38 +15,67 @@ export default async function Book({ id }: { id: string }) {
 
   return (
     <>
-      <div className="border-b border-gray-200 overflow-hidden relative">
-        <div className="w-full h-56 bg-gray-300 overflow-hidden flex justify-center items-center opacity-60">
-          <img className="w-full scale-150 blur" src={data.volumeInfo.imageLinks.thumbnail} alt="" />
+      <div className="relative overflow-hidden border-b border-gray-200">
+        <div className="flex h-56 w-full items-center justify-center overflow-hidden bg-gray-300 opacity-60">
+          <img
+            className="w-full scale-150 blur"
+            src={data.volumeInfo.imageLinks.thumbnail}
+            alt="a"
+          />
         </div>
-        <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+        <div className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]">
           {data.hasOwnProperty('volumeInfo') && data.volumeInfo.hasOwnProperty('imageLinks') && (
-            <img className="rounded-lg border border-gray-200" src={data.volumeInfo.imageLinks.thumbnail} width="128" height="168" />
+            <Image
+              className="h-auto w-28 rounded-lg border border-gray-200"
+              src={data.volumeInfo.imageLinks.thumbnail}
+              width={128}
+              height={168}
+              alt="thumbnail"
+            />
           )}
         </div>
       </div>
       <div className="px-4">
-        <div className="mt-8 text-xl text-slate-900 font-bold">{data.volumeInfo.title}</div>
+        <div className="mt-8 text-xl font-bold text-slate-900">{data.volumeInfo.title}</div>
+        {data.volumeInfo.hasOwnProperty('subtitle') && (
+          <div className="mt-1 text-sm text-gray-500">{data.volumeInfo.subtitle}</div>
+        )}
         <div className="mt-2">
           <LikeButton id={id} />
         </div>
-        {data.volumeInfo.hasOwnProperty('subtitle') && <div className="text-sm text-gray-500">{data.volumeInfo.subtitle}</div>}
 
-        <ul className="mt-4">
-          {data.volumeInfo.hasOwnProperty('publishedDate') && <li className="text-sm text-gray-500">ページ数{data.volumeInfo.pageCount}</li>}
-          {data.volumeInfo.hasOwnProperty('publishedDate') && <li className="text-sm text-gray-500">公開日: {data.volumeInfo.publishedDate}</li>}
-          {data.volumeInfo.hasOwnProperty('publisher') && <li className="text-sm text-gray-500">出版社: {data.volumeInfo.publisher}</li>}
-          {data.volumeInfo.hasOwnProperty('authors') && <li className="text-sm text-gray-500">寄稿者: {data.volumeInfo.authors}</li>}
+        <ul className="mt-4 flex flex-col gap-y-1">
+          {data.volumeInfo.hasOwnProperty('publishedDate') && (
+            <li className="text-sm text-gray-500">ページ数 : {data.volumeInfo.pageCount}</li>
+          )}
+          {data.volumeInfo.hasOwnProperty('publishedDate') && (
+            <li className="text-sm text-gray-500">公開日 : {data.volumeInfo.publishedDate}</li>
+          )}
+          {data.volumeInfo.hasOwnProperty('publisher') && (
+            <li className="text-sm text-gray-500">出版社 : {data.volumeInfo.publisher}</li>
+          )}
+          {data.volumeInfo.hasOwnProperty('authors') && (
+            <li className="text-sm text-gray-500">寄稿者 : {data.volumeInfo.authors}</li>
+          )}
         </ul>
 
         {data.saleInfo.hasOwnProperty('buyLink') && (
-          <a href={data.saleInfo.buyLink} target="_blank" className="inline-flex px-2 py-1 text-sm text-white bg-emerald-500 rounded">
-            Google Playで購入する
-          </a>
+          <div className="mt-4">
+            <a
+              href={data.saleInfo.buyLink}
+              target="_blank"
+              className="inline-flex justify-center rounded-lg bg-emerald-500 px-4 py-3 text-sm font-semibold text-white hover:bg-slate-700"
+            >
+              Google Playで購入する
+            </a>
+          </div>
         )}
 
-        <div>本の概要</div>
-        <div className="mt-4 text-sm text-[#344854]">{data.hasOwnProperty('volumeInfo') && data.volumeInfo.hasOwnProperty('description') && parse(data.volumeInfo.description)}</div>
+        {data.hasOwnProperty('volumeInfo') && data.volumeInfo.hasOwnProperty('description') && (
+          <div className="my-8 border-t border-slate-200">
+            <div className="mt-8 text-sm text-slate-600">{parse(data.volumeInfo.description)}</div>
+          </div>
+        )}
       </div>
     </>
   )
