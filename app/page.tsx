b269feede403679d06c4f6e033430fbc0a732f15
daxchx <daxchx.v1@gmail.com>
2024-02-12 02:27:07 +0900

    Fix img src path

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,80 @@
 import Link from 'next/link'
 
-export default function Home() {
+export default function RootPage() {
   return (
     <>
-      <div className="relative overflow-hidden z-10">
-        <img src="logo.svg" className="absolute z-[-1] w-[16rem] max-w-none left-[15rem] top-[-8rem]" />
-        <div className="pt-40 px-6">
-          <div className="font-medium text-md text-emerald-400">using Google Books API</div>
-          <div className="mt-4 font-extrabold text-4xl text-gray-900">本が見つかる</div>
-          <div className="text-lg text-gray-600 mt-4">
+      <div className="relative z-10 overflow-hidden">
+        <img
+          src="/logo.svg"
+          className="absolute left-[15rem] top-[-8rem] z-[-1] w-[16rem] max-w-none"
+        />
+        <div className="px-6 pt-40">
+          <div className="text-md font-medium text-emerald-400">using Google Books API</div>
+          <div className="mt-4 text-4xl font-extrabold text-gray-900">本が見つかる</div>
+          <div className="mt-4 text-lg text-gray-600">
             世界には本がたくさんあります。あなたが知っている本を検索して情報を確認することができます。
             <br />
           </div>
         </div>
         <div className="mt-8 px-6">
-          <Link href="/search" className="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700">
+          <Link
+            href="/search"
+            className="inline-flex justify-center rounded-lg bg-slate-900 px-4 py-3 text-sm font-semibold text-white hover:bg-slate-700"
+          >
             検索で本を探す
           </Link>
         </div>
-        <div className="flex flex-col gap-y-4 my-16">
+        <div className="my-16 flex flex-col gap-y-4">
           <div className="flex gap-x-4 px-6">
             <div className="shrink-0">
-              <img src="logo.svg" className="shadow-sm border border-gray-200 bg-gray-50 rounded w-10 p-2.5" />
+              <img
+                src="/logo.svg"
+                className="w-10 rounded border border-gray-200 bg-gray-50 p-2.5 shadow-sm"
+              />
             </div>
             <div className="">
               <h2 className="text-sm font-semibold leading-6 text-slate-900">検索はとっても簡単</h2>
-              <p className="mt-2 text-sm leading-6 text-slate-700">Each template is a well-structured Next.js project, giving you a codebase that’s productive and enjoyable to work in.</p>
+              <p className="mt-2 text-sm leading-6 text-slate-700">
+                Each template is a well-structured Next.js project, giving you a codebase that’s
+                productive and enjoyable to work in.
+              </p>
             </div>
           </div>
           <div className="flex gap-x-4 px-6">
             <div className="shrink-0">
-              <img src="logo.svg" className="shadow-sm border border-gray-200 bg-gray-50 rounded w-10 p-2.5" />
+              <img
+                src="/logo.svg"
+                className="w-10 rounded border border-gray-200 bg-gray-50 p-2.5 shadow-sm"
+              />
             </div>
             <div className="">
               <h2 className="text-sm font-semibold leading-6 text-slate-900">お気に入りを保存</h2>
-              <p className="mt-2 text-sm leading-6 text-slate-700">Each template is a well-structured Next.js project, giving you a codebase that’s productive and enjoyable to work in.</p>
+              <p className="mt-2 text-sm leading-6 text-slate-700">
+                Each template is a well-structured Next.js project, giving you a codebase that’s
+                productive and enjoyable to work in.
+              </p>
             </div>
           </div>
           <div className="flex gap-x-4 px-6">
             <div className="shrink-0">
-              <img src="logo.svg" className="shadow-sm border border-gray-200 bg-gray-50 rounded w-10 p-2.5" />
+              <img
+                src="/logo.svg"
+                className="w-10 rounded border border-gray-200 bg-gray-50 p-2.5 shadow-sm"
+              />
             </div>
             <div className="">
-              <h2 className="text-sm font-semibold leading-6 text-slate-900">Built with modern technologies</h2>
-              <p className="mt-2 text-sm leading-6 text-slate-700">Each template is a well-structured Next.js project, giving you a codebase that’s productive and enjoyable to work in.</p>
+              <h2 className="text-sm font-semibold leading-6 text-slate-900">
+                Built with modern technologies
+              </h2>
+              <p className="mt-2 text-sm leading-6 text-slate-700">
+                Each template is a well-structured Next.js project, giving you a codebase that’s
+                productive and enjoyable to work in.
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="border-t border-gray-200 h-40 bg-gray-50"></div>
+        <div className="h-40 border-t border-gray-200 bg-gray-50"></div>
       </div>
     </>
   )
