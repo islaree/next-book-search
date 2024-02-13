a61c05a03eeb9f9b2b113f71bfd5c33e3dbd98e2
daxchx <daxchx.v1@gmail.com>
2024-02-13 12:47:35 +0900

    Add new direcotry

---
+++ app/library/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import Book from '@/components/library/book'
+import { Suspense } from 'react'
+import { useDataContext } from '../../components/dataContext'
+import Loading from '@/components/loading'
+
+export default function LibraryPage() {
+  const { data, setData } = useDataContext()
+
+  return (
+    <>
+      <h1 className="px-6 py-10 text-lg font-bold">マイライブラリ</h1>
+      <div className="flex flex-col">
+        {data.map((id: string) => (
+          <div key={id} className="border-t border-gray-200 px-6 py-4">
+            <Suspense fallback={<Loading />}>
+              <Book id={id} />
+            </Suspense>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
