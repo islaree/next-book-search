bbaddad6b66ddc0b1b207d09b7c8a7c49d159d04
daxchx <daxchx.v1@gmail.com>
2024-02-23 15:19:08 +0900

    Add keyframes and animation to extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideToRight: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        slideToLeft: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        slideToRight: 'slideToRight 1s ease-in-out infinite',
+        slideToLeft: 'slideToLeft 1s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 }
