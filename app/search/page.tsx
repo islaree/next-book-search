a5c2b8907063cd4c697d41f73f4a73981a90f3a6
daxchx <daxchx.v1@gmail.com>
2024-09-19 23:24:00 +0900

    refactor: コンポーネント設計の見直し

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,26 +1,13 @@
-'use client'
-
-import { SetStateAction, Suspense, useState } from 'react'
-import Loading from '@/components/search/loading'
-import Search from '@/components/search/search'
-import Books from '@/components/search/books'
+import { BookList } from '@/components/book-list'
+import { SearchInput } from '@/components/search-bar'
 
 export default function SearchPage() {
-  const [value, setValue] = useState('')
-
-  const handleChange = (e: { target: { value: SetStateAction<string> } }) => {
-    setValue(e.target.value)
-  }
-
   return (
-    <div className="mx-auto w-full max-w-[1200px]">
-      <h1 className="mt-6 px-6 text-2xl font-bold text-slate-900">検索する</h1>
-      <Search value={value} onChange={handleChange} />
-      {value.length > 0 && (
-        <Suspense fallback={<Loading />}>
-          <Books keyword={value} />
-        </Suspense>
-      )}
+    <div className="space-y-4">
+      <div className="px-6">
+        <SearchInput />
+      </div>
+      <BookList />
     </div>
   )
 }
