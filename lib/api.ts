5e820f5245899328a21e9f6662aa2fd4cfa9bcab
daxchx <daxchx.v1@gmail.com>
2024-09-22 15:12:48 +0900

    feat: ロジックを追加する

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,31 @@
 export const getBooksData = async (term: string) => {
-  const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${term}`)
+  const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${term}&maxResults=40`)
   if (!res.ok) {
     throw new Error('Failed to fetch books data')
   }
+
+  const data = await res.json()
+
+  // 重複するidを排除
+  const uniqueBooks = data.items.reduce((acc: any[], current: any) => {
+    const x = acc.find((item) => item.id === current.id)
+    if (!x) {
+      return acc.concat([current])
+    } else {
+      return acc
+    }
+  }, [])
+
+  return { ...data, items: uniqueBooks }
+}
+
+export const getBookData = async (id: string) => {
+  const res = await fetch(`https://www.googleapis.com/books/v1/volumes/${id}`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch book data')
+  }
+
   const data = await res.json()
   return data
 }
