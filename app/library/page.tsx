ff9ab6298b30e8146ed21e3a36844a56c075fb22
daxchx <daxchx.v1@gmail.com>
2024-09-22 15:06:26 +0900

    refactor: 状態取得処理を削除

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,15 +1,10 @@
-'use client'
-
-import { useDataContext } from '../../components/context'
-import LibraryList from '@/components/library/library-list'
+import { LibraryList } from '@/components/library-list'
 
 export default function LibraryPage() {
-  const { data, setData } = useDataContext()
-
   return (
     <div className="mx-auto w-full max-w-[1200px]">
       <h1 className="mt-6 px-6 text-2xl font-bold text-slate-900">マイライブラリ</h1>
-      <LibraryList data={data} />
+      <LibraryList />
     </div>
   )
 }
