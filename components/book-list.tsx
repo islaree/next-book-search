a7969b11ccbd19e2642ff115659b9f4a589bfeb4
daxchx <daxchx.v1@gmail.com>
2024-09-22 15:08:43 +0900

    refactor: データ取得処理を変更

--- components/book-list.tsx
+++ components/book-list.tsx
@@ -1,26 +1,12 @@
-// 本のデータを表示する -> 本のデータを取得する
 'use client'
 
-import { useEffect, useState } from 'react'
 import { BookItem } from './book-item'
-import { bookItems } from '@/types/book'
-import { getBooksData } from '@/lib/api'
-import { useSearchStore } from '@/store/store'
+import { useBookStore } from '@/store/books'
 
 export const BookList = () => {
-  const [books, setBooks] = useState<bookItems[]>([])
-  const { searchTerm } = useSearchStore()
+  const { books } = useBookStore()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (searchTerm.trim().length > 0) {
-        const data = await getBooksData(searchTerm)
-        setBooks(data.items)
-      }
-    }
-
-    fetchData()
-  }, [searchTerm])
+  if (books.length == 0) return <p>検索してね</p>
 
   return (
     <div className="divide-y divide-gray-300">
